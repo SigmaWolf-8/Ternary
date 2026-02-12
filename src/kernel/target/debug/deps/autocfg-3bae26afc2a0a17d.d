f00cafac48e1146aa0edb7a71e7a940bd78c93c1
SigmaWolf-8 <Capomastro@Outlook.com>
2026-02-12 16:28:44 -0700

    fix(repo): remove 280MB build artifacts from tracking
    Removes 522 compiled Rust artifacts (.rlib, .rmeta, .so, .d files)
    from src/kernel/target/ that were tracked in git. These debug build
    outputs should never have been committed to version control.
    
    Finding NF-1 from Third-Party Engineering Review.

--- src/kernel/target/debug/deps/autocfg-3bae26afc2a0a17d.d
+++
@@ -1,9 +0,0 @@
-/home/runner/workspace/src/kernel/target/debug/deps/libautocfg-3bae26afc2a0a17d.rmeta: /home/runner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.1.0/src/lib.rs /home/runner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.1.0/src/error.rs /home/runner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.1.0/src/version.rs
-
-/home/runner/workspace/src/kernel/target/debug/deps/libautocfg-3bae26afc2a0a17d.rlib: /home/runner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.1.0/src/lib.rs /home/runner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.1.0/src/error.rs /home/runner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.1.0/src/version.rs
-
-/home/runner/workspace/src/kernel/target/debug/deps/autocfg-3bae26afc2a0a17d.d: /home/runner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.1.0/src/lib.rs /home/runner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.1.0/src/error.rs /home/runner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.1.0/src/version.rs
-
-/home/runner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.1.0/src/lib.rs:
-/home/runner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.1.0/src/error.rs:
-/home/runner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/autocfg-1.1.0/src/version.rs:
